56d9d440789a2c72973544cd0b2dfb90e2af9c3e
Design1Geek6 <Design.Geek@outlook.com>
2018-04-12 22:22:15 -0600

    Yippy I figured route and switch out based off of the Kithen Templet only Styling let to do

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from './Home'
 import Projects from './Projects'
 import JobHistory from './JobHistory'
 import './css/tyler.css'
+import { BrowserRouter as Router, Route, NavLink as Link, Switch } from 'react-router-dom' //in terminal install via "npm install react-router-dom"
+
 
 export default class extends Component {
   constructor(){
@@ -19,10 +21,29 @@ export default class extends Component {
     console.log('App.render()')
 
     return (
-      <div>
-        <Home/> <Projects /> <JobHistory/>
-      </div>
+      <Router>
+        <div className="App">
+          <nav>
+            <Link exact to="/" activeStyle={{ fontWeight: 'bold', color: 'red' }}>Projects</Link>
+            <Link exact to="/JobHistory" activeStyle={{ fontWeight: 'bold', color: 'red' }}>Work History</Link>
+            <Link to="/Home" activeStyle={{ fontWeight: 'bold', color: 'red' }}>Home</Link>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={Projects} />
+            <Route exact path="/JobHistory" component={JobHistory} />
+            <Route path="/Home" component={() => <Home />} />
+            <Route component={() => <div>404 ERROR</div>} />
+          </Switch>
+        </div>
+      </Router>
     )
+
+    // return (
+    //   <div>
+    //     <Home/> <Projects /> <JobHistory/>
+
+    //   </div>
+    // )
   }
 
   componentDidMount(){
